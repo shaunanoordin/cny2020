5ade7de7f02caaa2f4d67bba1dc56b8ba4f483a8
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2019-12-26 00:23:44 +0000

    Add canvas grid

--- src/cny2020/cny2020.js
+++ src/cny2020/cny2020.js
@@ -1,3 +1,5 @@
+import { TILE_SIZE, GRID_WIDTH, GRID_HEIGHT } from './constants'
+
 class CNY2020 {
   constructor () {
     this.html = {
@@ -6,7 +8,12 @@ class CNY2020 {
       console: document.getElementById('console'),
     };
     this.canvas2d = this.html.canvas.getContext('2d');
+    this.canvasWidth = TILE_SIZE * GRID_WIDTH;
+    this.canvasHeight = TILE_SIZE * GRID_HEIGHT;
     this.messages = [];
+    
+    this.html.canvas.width = this.canvasWidth;
+    this.html.canvas.height = this.canvasHeight;
     
     this.prevTime = null;
     this.nextFrame = window.requestAnimationFrame(this.main.bind(this));
@@ -16,7 +23,27 @@ class CNY2020 {
     const timeDelta = (this.prevTime) ? time - this.prevTime : time;
     this.prevTime = time;
     
+    this.play(timeDelta);
+    this.paint();
+    
     this.nextFrame = window.requestAnimationFrame(this.main.bind(this));
+  }
+  
+  play (timeDelta) {
+    
+  }
+  
+  paint () {
+    this.canvas2d.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    
+    this.canvas2d.strokeStyle = '2px solid #888';
+    this.canvas2d.beginPath();
+    for (let y = 0; y < GRID_HEIGHT; y++) {
+      for (let x = 0; x < GRID_HEIGHT; x++) {
+        this.canvas2d.rect(TILE_SIZE * x, TILE_SIZE * y, TILE_SIZE, TILE_SIZE);
+      }
+    }
+    this.canvas2d.stroke();
   }
   
   print (text) {
---
+++ src/cny2020/constants.js
@@ -0,0 +1,3 @@
+export const TILE_SIZE = 64;
+export const GRID_WIDTH = 9;
+export const GRID_HEIGHT = 7;

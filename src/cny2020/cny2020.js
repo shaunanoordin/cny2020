0e6f8595d1dc5f7cab1f78bab1f44a8182d343e6
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2019-12-26 00:03:46 +0000

    Add basic print function, testing grid sizes

--- src/cny2020/cny2020.js
+++ src/cny2020/cny2020.js
@@ -1,7 +1,15 @@
 class CNY2020 {
   constructor () {
+    this.html = {
+      app: document.getElementById('app'),
+      canvas: document.getElementById('canvas'),
+      console: document.getElementById('console'),
+    };
+    this.canvas2d = this.html.canvas.getContext('2d');
+    this.messages = [];
+    
     this.prevTime = null;
-    this.main();
+    this.nextFrame = window.requestAnimationFrame(this.main.bind(this));
   }
   
   main (time) {
@@ -9,6 +17,15 @@ class CNY2020 {
     this.prevTime = time;
     
     this.nextFrame = window.requestAnimationFrame(this.main.bind(this));
+  }
+  
+  print (text) {
+    this.messages.unshift(text);
+    while (this.messages.length > 3) {
+      this.messages.pop();
+    }
+    
+    this.html.console.textContent = this.messages.join('\n');
   }
 };
 

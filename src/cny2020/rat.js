2e816623b2dffdc22d2c4ea3ff14b6c3f18408d0
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-01-12 00:38:15 +0000

    Debug draw rat direction

--- src/cny2020/rat.js
+++ src/cny2020/rat.js
@@ -12,17 +12,42 @@ class Rat {
   }
   
   paint (canvas2d, leftPadding, topPadding) {
-    console.log(this.x, this.y);
-    
     // TODO
+    const offsetX = (this.x - this.fromX) * this.movePercentage;
+    const offsetY = (this.y - this.fromY) * this.movePercentage;
+    const x = this.x + offsetX + leftPadding + 0.5;
+    const y = this.y + offsetY + topPadding + 0.5;
+    
     canvas2d.fillStyle = '#c44';
     canvas2d.beginPath();
     canvas2d.arc(
-      (this.x + leftPadding + 0.5) * TILE_SIZE,
-      (this.y + topPadding + 0.5) * TILE_SIZE,
+      x * TILE_SIZE,
+      y * TILE_SIZE,
       TILE_SIZE / 3,
       0, 2 * Math.PI);
     canvas2d.fill();
+
+    canvas2d.strokeStyle = '#fcc';
+    canvas2d.lineWidth = 4;
+    canvas2d.beginPath();
+    canvas2d.moveTo(x * TILE_SIZE, y * TILE_SIZE);
+    console.log(this.direction);
+    switch (this.direction) {
+      case DIRECTIONS.SOUTH:
+        canvas2d.lineTo(x * TILE_SIZE, (y + 0.3) * TILE_SIZE);
+        break;
+      case DIRECTIONS.EAST:
+        canvas2d.lineTo((x + 0.3) * TILE_SIZE, y * TILE_SIZE);
+        break;
+      case DIRECTIONS.NORTH:
+        canvas2d.lineTo(x * TILE_SIZE, (y - 0.3) * TILE_SIZE);
+        break;
+      case DIRECTIONS.WEST:
+        canvas2d.lineTo((x - 0.3) * TILE_SIZE, y * TILE_SIZE);
+        break;
+    }
+    canvas2d.stroke();
+    
   }
 }
 

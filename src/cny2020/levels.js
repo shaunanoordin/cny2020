b41f2331c67fc95a2333876fbfd1cc9aeb5e25fc
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-01-19 03:56:39 +0000

    8 levels

--- src/cny2020/levels.js
+++ src/cny2020/levels.js
@@ -203,6 +203,115 @@ export function getLevel(level) {
           direction: DIRECTIONS.EAST,
         },
       });
+      
+    case 6:  // Super challenge
+      return new Grid({
+        width: 11,
+        height: 5,
+        tiles: [
+          [
+            new Tile({ e: true, s: true, }),
+            new Tile({ }),
+            new Tile({ w: true, e: true, }),
+            new Tile({ w: true, s: true, canMove: false, }),
+            new Tile({ e: true, n: true, }),
+            new Tile({ e: true, s: true, }),
+            null,
+            new Tile({ e: true, w: true, canMove: false, }),
+            new Tile({ e: true, n: true, }),
+            new Tile({ w: true, e: true, }),
+            new Tile({ }),
+          ],
+          [
+            null,
+            new Tile({ w: true, e: true, }),
+            new Tile({ w: true, e: true, }),
+            new Tile({ w: true, n: true, canMove: false, }),
+            new Tile({ s: true, w: true, }),
+            new Tile({ w: true, n: true, }),
+            new Tile({ w: true, e: true, }),
+            new Tile({ w: true, s: true, canMove: false, }),
+            new Tile({ s: true, w: true, }),
+            new Tile({ s: true, w: true, }),
+            null,
+          ],
+          [
+            new Tile({ n: true, s: true, canMove: false, }),
+            new Tile({ canMove: false, }),
+            new Tile({ s: true, e: true, canMove: false, }),
+            new Tile({ w: true, s: true, canMove: false, }),
+            new Tile({ canMove: false, }),
+            new Tile({ canMove: false, }),
+            new Tile({ e: true, s: true, canMove: false, }),
+            new Tile({ n: true, e: true, w: true, s: true, canMove: false, }),
+            new Tile({ w: true, s: true, canMove: false, }),
+            new Tile({ canMove: false, }),
+            new Tile({ s: true, n: true, canMove: false, }),
+          ],
+          [
+            null,
+            new Tile({ s: true, n: true, }),
+            new Tile({ w: true, n: true, }),
+            new Tile({ n: true, s: true, canMove: false, }),
+            new Tile({ s: true, e: true, }),
+            new Tile({ }),
+            new Tile({ e: true, w: true, }),
+            new Tile({ n: true, canMove: false, goal: true, }),
+            null,
+            new Tile({ n: true, s: true, }),
+            new Tile({ e: true, w: true, }),
+          ],
+          [
+            new Tile({ n: true, e: true, }),
+            new Tile({ e: true, s: true, }),
+            new Tile({ w: true, n: true, }),
+            new Tile({ n: true, e: true, canMove: false, }),
+            null,
+            new Tile({ w: true, n: true, }),
+            new Tile({ w: true, n: true, }),
+            new Tile({ canMove: false, }),
+            new Tile({ n: true, e: true, }),
+            new Tile({ w: true, n: true, }),
+            new Tile({ n: true, s: true, }),
+          ],
+        ],
+        rat: {
+          x: 0,
+          y: 0,
+          direction: DIRECTIONS.EAST,
+        },
+      });
+    
+    case 7:  // Original level, remixed for final
+      return new Grid({
+        width: 3,
+        height: 3,
+        tiles: [
+          [
+            new Tile({ south: true, canMove: false, }),
+            new Tile({ }),
+            new Tile({ south: true, goal: true, }),
+          ],
+          [
+            null,
+            new Tile({ south: true, north: true, }),
+            new Tile({ south: true, east: true, }),
+          ],
+          [
+            new Tile({ north: true, east: true, }),
+            new Tile({ west: true, north: true, }),
+            new Tile({ west: true, south: true, }),
+          ],
+        ],
+        rat: {
+          x: 0,
+          y: 0,
+          direction: DIRECTIONS.SOUTH,
+        },
+      });
+    
+    // The Lost Levels
+    // --------------------------------
     
     case 370:  // TNC's level
       return new Grid({
@@ -250,6 +359,84 @@ export function getLevel(level) {
           y: 0,
           direction: DIRECTIONS.SOUTH,
         }
+      });
+    
+    case 100:  // Troll level
+      return new Grid({
+        width: 11,
+        height: 5,
+        tiles: [
+          [
+            new Tile({ }),
+            new Tile({ s: true, e: true }),
+            new Tile({ e: true, w: true }),
+            new Tile({ w: true, }),
+            new Tile({ s: true, e: true, }),
+            new Tile({ w: true, s: true}),
+            new Tile({ s: true, }),
+            new Tile({ s: true, }),
+            new Tile({ }),
+            new Tile({ s: true, }),
+            new Tile({ }),
+          ],
+          [
+            new Tile({ }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ }),
+            new Tile({ }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ n: true, e: true, }),
+            new Tile({ w: true, s: true, e: true, }),
+            new Tile({ n: true, w: true, }),
+            new Tile({ }),
+          ],
+          [
+            new Tile({ e: true, canMove: false, }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ }),
+            new Tile({ }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ }),
+            new Tile({ s: true, n: true, }),
+            null,
+            new Tile({ goal: true, w: true }),
+          ],
+          [
+            new Tile({ }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ }),
+            new Tile({ }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ }),
+            new Tile({ s: true, n: true, }),
+            new Tile({ }),
+            new Tile({ }),
+          ],
+          [
+            new Tile({ }),
+            new Tile({ e: true, n: true, }),
+            new Tile({ e: true, w: true, }),
+            new Tile({ w: true, }),
+            new Tile({ n: true, }),
+            new Tile({ n: true, e: true, }),
+            new Tile({ n: true, w: true, }),
+            new Tile({ }),
+            new Tile({ n: true, }),
+            new Tile({ }),
+            new Tile({ }),
+          ],
+        ],
+        rat: {
+          x: 0,
+          y: 2,
+          direction: DIRECTIONS.EAST,
+        },
       });
   }
   
